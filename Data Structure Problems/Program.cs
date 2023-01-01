66de521a1f980c1faa228392f557cde5ebb3f2d0
Raojyoti <117572975+RaoJyoti123@users.noreply.github.com>
2023-01-01 22:13:01 +0530

    [Jyoti Rao] Search: Found word in list

---
+++ Data Structure Problems/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Drawing;
+using System.Threading;
+
+namespace Data_Structure_Problems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList<string> linkedList = new LinkedList<string>();
+            string text = "Today is not sunday";
+            string[] list = text.Split(" ");
+            int count = 0;
+            while (count < list.Length)
+            {
+                linkedList.Add(list[count++]);
+            }
+            linkedList.Display();
+            linkedList.Search("is");
+            
+        }
+    }
+}
